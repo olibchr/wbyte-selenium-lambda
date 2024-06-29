9caf40d1529764c688d569278c8ccc3ae350d86f
Oliver Becher <oliverbecher@velocityglobal.com>
2024-06-29 15:16:18 +0100

    update

--- _CI/bootstrap.sh
+++ _CI/bootstrap.sh
@@ -11,6 +11,14 @@ if [[ -d "infra" ]]; then
 
     echo "Synthesize infra.."
 
+    npm run cdk bootstrap -- \
+        --quiet \
+        --context name=${APPLICATION_NAME} \
+        --context accountId=${AWS_ACCOUNT_ID} \
+        --context region=${AWS_REGION} \
+        --context apiKey=${API_KEY} \
+        --context applicationTag=${APPLICATION_TAG}
+
     npm run cdk synth -- \
         --quiet \
         --context name=${APPLICATION_NAME} \
--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import logging
 import json
+import time
+import datetime
 
 from tempfile import mkdtemp
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import telegram
+import asyncio
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 
 def initialise_driver():
     chrome_options = ChromeOptions()
@@ -39,22 +44,68 @@ def initialise_driver():
 
     return driver
 
+def main(driver) -> None:
+    print(datetime.datetime.now())
+    print("Starting")
+    msg = ""
+    try:
+        print("driver")
+        driver.get("https://visas-de.tlscontact.com/visa/ie/ieDUB2de/home")
+        asyncio.sleep(1)
+        elem = driver.find_element(By.XPATH, "//*[contains (text(),'Login')]")
+        elem.click()
+        print("Found login")
+        asyncio.sleep(5)
+        elem = driver.find_element(By.NAME, "username")
+        elem.clear()
+        elem.send_keys("name")
+        elem = driver.find_element(By.NAME, "password")
+        elem.clear()
+        elem.send_keys("pw")
+        elem.send_keys(Keys.RETURN)
+        print("Managed login")
+        asyncio.sleep(2)
+        driver.get("https://visas-de.tlscontact.com/appointment/ie/ieDUB2de/2524620")
+        print("Appointments")
+        asyncio.sleep(15)
+        print("sending message")
+        if "Sorry, there is no available appointment at the moment, please check again later." not in driver.page_source:
+            print("appointment found")
+            msg = "appointment found"
+        else:
+            msg = "nothing"
+            print("nothing")
+    except Exception as e:
+        print(str(e))
+        msg = 'Error + ' + str(e)
+    print("finished")
+    driver.close()
+    return msg
+
+async def send_ms(msg):
+    bot = telegram.Bot("6469377202:AAE2-tphnAU3uzRyfuEMsYnbvnI25H2jhFo")
+    print("got bot and sending : " + str(msg))
+    with bot:
+        await bot.send_message(text=msg, chat_id=13446158)
+        time.sleep(1)
 
 def lambda_handler(event, context):
-    driver = initialise_driver()
-    driver.get("https://wbyte.dev")
-    logger.info(f"Page title: ${driver.title}")
-
-    body = {
-        "title": driver.title
-    }
+    msg = main(initialise_driver())
+    asyncio.run(send_ms(msg))
 
     response = {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps(body)
+        "body": json.dumps(msg)
     }
 
     return response
+
+if __name__ == '__main__':
+    from selenium.webdriver.firefox.options import Options as FirefoxOptions
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    msg = main(driver = webdriver.Firefox(options=options))
+    asyncio.run(send_ms(msg))
